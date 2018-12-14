de6f26260cb878a5c75f19824bf6431bdee866c5
David Hernández <david.vzla@gmail.com>
2018-12-14 08:16:50 -0400

    Se añade Encuentro Ciudadano

--- ppvclad.sh
+++ ppvclad.sh
@@ -2,6 +2,11 @@
 
 gnuclad ppv.csv svg ppv.conf
 
+# Partido Revolucionario Nacionalista «PRN»
 sed -i -e s/'x=\x273221\x27 y=\x27547/x=\x273540\x27 y=\x27547/' ppv.svg
-sed -i -e s/'x=\x273845\x27 y=\x273022/x=\x274110\x27 y=\x273022/' ppv.svg
-sed -i -e s/'x=\x274213\x27 y=\x273022/x=\x274360\x27 y=\x273022/' ppv.svg
+
+# Unión Progresista del Pueblo «UPP»
+sed -i -e s/'x=\x273845\x27 y=\x273047/x=\x274110\x27 y=\x273047/' ppv.svg
+
+# Frente Justicialista de Liberación «FREJULI»
+sed -i -e s/'x=\x274213\x27 y=\x273047/x=\x274360\x27 y=\x273047/' ppv.svg
